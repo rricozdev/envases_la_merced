2c3b857c36af4e598158378c7eb6e3c7d29d1237
Shago01 <kgsanty2001@gmail.com>
2025-12-13 15:57:07 -0500

    feat: Integrar contexto del carrito en el encabezado y mejorar visualización en el carrito

--- client/src/components/common/cart/CartDrawer.jsx
+++ client/src/components/common/cart/CartDrawer.jsx
@@ -12,19 +12,19 @@ export default function CartDrawer() {
   return (
     <aside
       className={`
-        fixed top-0 right-0 z-50
-        h-screen  sm:w-96
-        bg-bgligth-main dark:bg-bgdark-tertiary
-        shadow-2xl
-        flex flex-col
-        transition-transform duration-300 ease-in-out
-        ${open ? "translate-x-0" : "translate-x-full"}
+      fixed top-0 right-0 z-50
+      h-dvh w-full sm:w-96
+      bg-bgligth-main dark:bg-bgdark-tertiary
+      shadow-2xl
+      flex flex-col
+      transition-transform duration-300 ease-in-out
+      ${open ? "translate-x-0" : "translate-x-full"}
       `}
       aria-hidden={!open}
     >
-      <CartHeader />
+      <CartHeader className="shrink-0" />
       <CartList />
-      <CartFooter />
+      <CartFooter className="shrink-0" />
     </aside>
   );
 }
