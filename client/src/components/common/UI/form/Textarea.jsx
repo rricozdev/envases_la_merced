b6f4fe65fc87b6253920e95ae2a9dbb52da2a9d3
Shago01 <kgsanty2001@gmail.com>
2025-12-14 16:24:05 -0500

    feat: agregar componentes Input y Textarea al formulario

---
+++ client/src/components/common/UI/form/Textarea.jsx
@@ -0,0 +1,22 @@
+export default function Textarea({ label, error, ...props }) {
+  return (
+    <label className="flex flex-col">
+      <span className="pb-2 text-base font-medium text-secondary dark:text-dark-text">
+        {label}
+      </span>
+
+      <textarea
+        {...props}
+        className={`form-textarea min-h-40 outline-none resize-y rounded-lg border p-[15px]
+          ${
+            error
+              ? "border-red-500 focus:border-red-500/50"
+              : "border-[#dbe0e6] dark:border-[#364554] focus:border-brand-accent-hover"
+          }
+          bg-white  dark:bg-neutral-900/50`}
+      />
+
+      {error && <span className="mt-1 text-sm text-red-600">{error}</span>}
+    </label>
+  );
+}
