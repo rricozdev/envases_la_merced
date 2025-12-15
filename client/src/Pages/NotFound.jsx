4205af53bf0b37d84ba2d416730b96a02ed877eb
Shago01 <kgsanty2001@gmail.com>
2025-12-14 20:34:27 -0500

    correción de importación Button

--- client/src/Pages/NotFound.jsx
+++ client/src/Pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Button from "../common/UI/Button";
+import Button from "../components/UI/Button";
 
 export default function NotFound() {
   return (
