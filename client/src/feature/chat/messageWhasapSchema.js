06ab408c54c0245b25dd0413c0986da734b40ce2
Shago01 <kgsanty2001@gmail.com>
2025-12-20 20:21:18 -0500

    mejora de formato en el mensaje de WhatsApp y eliminación de atributo innecesario en el Header

--- client/src/feature/chat/messageWhasapSchema.js
+++ client/src/feature/chat/messageWhasapSchema.js
@@ -1,16 +1,16 @@
 export function messageWhasapSchemaCartProduct(products) {
-  let msg = "*Hola,*" + "Estoy interesado en cotizar los siguientes productos:";
+  let msg = "*Hola*," + "Estoy interesado en cotizar los siguientes productos:";
 
   products.forEach((product, index) => {
-    msg +=
-      `*${index + 1}. ${product.nombre}*` +
-      `${product.volumen ? ` · ${product.volumen} ml` : ""}` +
-      `- *Cantidad:* ${product.quantity}` +
-      `${
+    msg += `*${index + 1}. ${product.nombre}*
+      ${product.volumen ? ` · ${product.volumen} ml` : ""}
+       - *Cantidad:* ${product.quantity}
+      ${
         product.piezasPorPaquete
           ? `- *PZS / Paq:* ${product.piezasPorPaquete}`
           : ""
-      }`;
+      }
+      `;
   });
 
   msg += "Quedo atento(a) a su respuesta. ¡Gracias!";
