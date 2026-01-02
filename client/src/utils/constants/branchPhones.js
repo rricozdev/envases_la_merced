819f55b10a672a2c07d0d581169e3324e948ba6c
rricoz <106559283+rricozdev@users.noreply.github.com>
2026-01-02 12:14:05 -0500

    update app, añadiendo email a sucursales

--- client/src/utils/constants/branchPhones.js
+++ client/src/utils/constants/branchPhones.js
@@ -2,25 +2,30 @@ export const BRANCH_PHONES = {
   CDMX: {
     phones: ["525576676045", "525591292477", "525591340732"],
     whatsapp: "+5215576576045",
+    email: ["grangas@envaseslamerced.mx"],
   },
 
   PUEBLA: {
     phones: ["522211460293", "522229126345"],
     whatsapp: "+5212211460293",
+    email: ["puebla@envaseslamerced.mx"],
   },
 
   VERACRUZ: {
     phones: ["522295265435"],
     whatsapp: "+5212295265435",
+    email: ["veracruz@envaseslamerced.mx"],
   },
 
   NEZA: {
     phones: ["525521528498"],
     whatsapp: "+5215521528498",
+    email: ["neza@envaseslamerced.mx"],
   },
 
   QUERETARO: {
     phones: ["524421418651"],
     whatsapp: "+5214421418651",
+    email: ["queretaro@envaseslamerced.mx"],
   },
 };
