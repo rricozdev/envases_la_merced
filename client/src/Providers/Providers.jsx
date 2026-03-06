9b4b1898517bab3f74588377b5d0d0bb51b5a8d4
= <=>
2026-03-05 23:29:24 -0500

    feat: add helmet al provedor general

--- client/src/Providers/Providers.jsx
+++ client/src/Providers/Providers.jsx
@@ -3,33 +3,35 @@ import router from "../routes/routerConfig";
 import { ThemeProvider } from "./ThemeProvider";
 import { CartProvider } from "./CartProvider";
 import { UIProvider } from "./UIProvider";
-
+import { HelmetProvider } from "react-helmet-async";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Providers() {
   return (
-    <ThemeProvider>
-      <UIProvider>
-        <CartProvider>
-          <RouterProvider router={router} />
-          {/* Toasts globales */}
-          <ToastContainer
-            position="top-right"
-            autoClose={4000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            pauseOnHover
-            draggable
-            theme="light"
-            toastClassName={() =>
-              "bg-white text-gray-800 text-sm rounded-lg shadow-lg px-4 py-3"
-            }
-            progressClassName="bg-brand-accent"
-          />
-        </CartProvider>
-      </UIProvider>
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider>
+        <UIProvider>
+          <CartProvider>
+            <RouterProvider router={router} />
+            {/* Toasts globales */}
+            <ToastContainer
+              position="top-right"
+              autoClose={4000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              pauseOnHover
+              draggable
+              theme="light"
+              toastClassName={() =>
+                "bg-white text-gray-800 text-sm rounded-lg shadow-lg px-4 py-3"
+              }
+              progressClassName="bg-brand-accent"
+            />
+          </CartProvider>
+        </UIProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
